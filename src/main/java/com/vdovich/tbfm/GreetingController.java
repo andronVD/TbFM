72ee256c814656a4ae7f191e232d660d009fd5b1
Soprativlenie <zlykabober84@gmail.com>
2019-11-11 11:50:48 +0200

    Changes according to PR discussion

--- src/main/java/com/vdovich/tbfm/GreetingController.java
+++ src/main/java/com/vdovich/tbfm/GreetingController.java
@@ -2,16 +2,17 @@ package com.vdovich.tbfm;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RequestMapping
+@RestController
 public class GreetingController {
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping
-    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World")String name){
-        return new Greeting(counter.incrementAndGet(),String.format(TEMPLATE,name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 }
