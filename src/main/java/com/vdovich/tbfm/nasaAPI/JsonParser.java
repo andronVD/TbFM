3fc91950ede7ba5c89eee2b9e7862ea3f90e4062
Soprativlenie <zlykabober84@gmail.com>
2019-12-06 13:42:20 +0200

    WIP. Implemented saving picture

--- src/main/java/com/vdovich/tbfm/nasaAPI/JsonParser.java
+++ src/main/java/com/vdovich/tbfm/nasaAPI/JsonParser.java
@@ -4,9 +4,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonParser {
-    public String getPicture(String response) throws JSONException {
+    public static String getPicture(String response) throws JSONException {
         JSONObject jsonObject = new JSONObject(response);
-        String pictureUrl = jsonObject.getString("url");
-        return pictureUrl;
+        return jsonObject.getString("url");
     }
 }
--- src/main/java/com/vdovich/tbfm/nasaAPI/NasaController.java
+++ src/main/java/com/vdovich/tbfm/nasaAPI/NasaController.java
@@ -1,9 +1,8 @@
 package com.vdovich.tbfm.nasaAPI;
 
-import java.awt.*;
+
 import java.io.*;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.ImageIO;
 
 @RestController
 @RequestMapping("/nasa")
@@ -19,23 +17,30 @@ public class NasaController {
     //TODO make url enum
     private static final String API_URL = "https://api.nasa.gov/planetary/apod";
     private static final String API_KEY_QUERY_PARAM = "api_key";
+    private static final String DESTINATION_FILE = "/home/user/image.jpg";
     @Value("${nasa.api.token}")
     private String nasaApiToken;
 
-    JsonParser jsonParser = new JsonParser();
 
     @GetMapping("/pictureOfTheDay")
     public String callNasaApi() throws IOException, JSONException {
-        StringBuilder result = new StringBuilder();
-
-
         //TODO: use url builder
-        InputStream in = new URL(API_URL + "?" + API_KEY_QUERY_PARAM + "=" + nasaApiToken).openStream();
-        String response = convertStreamToString(in);
-        String picture = jsonParser.getPicture(response);
-//        ImageIO.read(new URL(picture)).;
-        return picture;
+        InputStream inputStream = new URL(API_URL + "?" + API_KEY_QUERY_PARAM + "=" + nasaApiToken).openStream();
+        String response = convertStreamToString(inputStream);
+        return JsonParser.getPicture(response);
+    }
 
+    @GetMapping("/fullpicture")
+    public void savePicture() throws IOException, JSONException {
+        InputStream is = new URL(callNasaApi()).openStream();
+        OutputStream os = new FileOutputStream(DESTINATION_FILE);
+        byte[] b = new byte[2048];
+        int length;
+        while ((length = is.read(b)) != 1) {
+            os.write(b, 0, length);
+        }
+        is.close();
+        os.close();
     }
 
     private String convertStreamToString(InputStream stream) throws IOException {
