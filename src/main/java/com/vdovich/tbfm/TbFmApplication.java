92a9cbcb4c40a22657a8d521c7bdffabb386a393
Soprativlenie <zlykabober84@gmail.com>
2020-01-03 16:13:57 +0200

    #3 Create bot, instantiate TelegramBotsApi and register new bot, Implement bot answer("Hello, my brooo!")

--- src/main/java/com/vdovich/tbfm/TbFmApplication.java
+++ src/main/java/com/vdovich/tbfm/TbFmApplication.java
@@ -1,13 +1,21 @@
 package com.vdovich.tbfm;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 public class TbFmApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TbFmApplication.class, args);
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
     }
 
 }
