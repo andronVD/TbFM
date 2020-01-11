6c3cf4fa1e18e13d66f4d2843e6227f9339ee2fe
Soprativlenie <zlykabober84@gmail.com>
2020-01-11 11:54:06 +0200

    deleted unnecessary class

--- src/main/java/com/vdovich/tbfm/service/Snippet.java
+++
@@ -1,8 +0,0 @@
-package com.vdovich.tbfm.service;
-
-public class Snippet {
-	private static final String API_URL = "https://api.nasa.gov/planetary/apod";
-	    private static final String API_KEY_QUERY_PARAM = "api_key";
-	    private static final String DESTINATION_FILE = "/home/user/image.jpg";
-}
-
