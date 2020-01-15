08745ed7247a456c24177f5a3b09d72ec0ff71c0
Soprativlenie <zlykabober84@gmail.com>
2020-01-15 15:15:33 +0200

    #3 WIP Implemented Json parser for two Json object, deleted duplicate dependency in pom

--- src/main/java/com/vdovich/tbfm/util/JsonParser.java
+++ src/main/java/com/vdovich/tbfm/util/JsonParser.java
@@ -9,15 +9,21 @@ public class JsonParser {
     public static String getPicture(String response) {
         com.google.gson.JsonParser parser = new com.google.gson.JsonParser();
         String src = "";
-        String src_url = "";
         JsonObject o = parser.parse(response).getAsJsonObject();
-        JsonArray arr = o.getAsJsonArray("latest_photos");
-        for (int i = 0; i < arr.size(); i++) {
-
-            src = arr.get(i).getAsJsonObject().get("img_src").getAsString();
-//            src_url = arr.get(i).getAsJsonObject().get("url").getAsString();
+        try {
+            src = o.get("url").getAsString();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
-//        return src == null ? src_url : src;
+        try {
+            JsonArray arr = o.getAsJsonArray("latest_photos");
+            for (int i = 0; i < arr.size(); i++) {
+                src = arr.get(i).getAsJsonObject().get("img_src").getAsString();
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
         return src;
     }
 }
+
