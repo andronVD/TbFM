01cf361bd3c414419553f384b900b7236d0dd24b
Soprativlenie <zlykabober84@gmail.com>
2020-01-15 15:28:58 +0200

    #3 WIP Minor changes

--- src/main/java/com/vdovich/tbfm/util/JsonParser.java
+++ src/main/java/com/vdovich/tbfm/util/JsonParser.java
@@ -4,24 +4,25 @@ package com.vdovich.tbfm.util;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class JsonParser {
+    private static final Logger logger = LoggerFactory.getLogger(JsonParser.class);
+
     public static String getPicture(String response) {
+
         com.google.gson.JsonParser parser = new com.google.gson.JsonParser();
         String src = "";
         JsonObject o = parser.parse(response).getAsJsonObject();
         try {
             src = o.get("url").getAsString();
         } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-        try {
             JsonArray arr = o.getAsJsonArray("latest_photos");
             for (int i = 0; i < arr.size(); i++) {
                 src = arr.get(i).getAsJsonObject().get("img_src").getAsString();
             }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
         }
         return src;
     }
